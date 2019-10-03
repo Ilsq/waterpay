f1032522d256cb9bf8324dfc9d295a19409ff3ef
Ziganshin Ildus <ilsq@mail.ru>
2019-10-03 17:21:02 +0300

    Enter to user account

--- src/main/java/hello/AccountController.java
+++ src/main/java/hello/AccountController.java
@@ -4,7 +4,6 @@ import entities.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +18,7 @@ public class AccountController {
 
     @PostMapping(path = "/add")
     public ModelAndView addNewAccount(@RequestParam(name = "name") String login, @RequestParam(name = "pass") String password, ModelMap model) {
+        model.addAttribute("attribute", "redirectWithRedirectPrefix");
         if (accountRepository.findByLogin(login) != null) {
             return new ModelAndView("redirect:/register.html", model);
         }
@@ -26,12 +26,23 @@ public class AccountController {
         account.setLogin(login);
         account.setPassword(password);
         accountRepository.save(account);
-        model.addAttribute("attribute", "redirectWithRedirectPrefix");
         return new ModelAndView("redirect:/login.html", model);
     }
 
-    @GetMapping(path = "/all")
-    public Iterable<Account> getAllAccounts() {
-        return accountRepository.findAll();
+//    @GetMapping(path = "/all")
+//    public Iterable<Account> getAllAccounts() {
+//        return accountRepository.findAll();
+//    }
+
+    @PostMapping(path = "/enter")
+    public ModelAndView enterAccount(@RequestParam(name = "name") String login, @RequestParam(name = "pass") String password, ModelMap model) {
+        model.addAttribute("attribute", "redirectWithRedirectPrefix");
+        if (accountRepository.findByLogin(login) == null) {
+            return new ModelAndView("redirect:/login.html", model);
+        }
+        if (!accountRepository.findByLogin(login).getPassword().equals(password)) {
+            return new ModelAndView("redirect:/login.html", model);
+        }
+        return new ModelAndView("redirect:/fastorder.html", model);
     }
 }
