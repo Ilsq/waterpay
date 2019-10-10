46d7463877822ece4ca2201feaf146e912f52d19
Ziganshin Ildus <ilsq@mail.ru>
2019-10-10 18:08:16 +0300

    output name in paid page

--- src/main/java/org/coworking/ilsq/controller/PaidController.java
+++ src/main/java/org/coworking/ilsq/controller/PaidController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,7 +12,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class PaidController {
 
     @PostMapping
-    public ModelAndView goToLogin(ModelMap model) {
+    public ModelAndView goToLogin(@RequestParam(name = "name") String name, ModelMap model) {
+        model.addAttribute("name", name);
         return new ModelAndView("paid", model);
     }
 }
