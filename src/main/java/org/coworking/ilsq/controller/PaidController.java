8e9fbfde8eea2d715be3d2a851adc27949df3d6d
Ziganshin Ildus <ilsq@mail.ru>
2020-01-17 14:41:02 +0300

    fixes'''''''

--- src/main/java/org/coworking/ilsq/controller/PaidController.java
+++ src/main/java/org/coworking/ilsq/controller/PaidController.java
@@ -30,23 +30,18 @@ public class PaidController {
     public ModelAndView goToPaid(@RequestParam(name = "name") String name, ModelMap model) {
 
         Optional<Levy> last = levyRepository.findFirstByOrderByIdDesc();
-
-        if (!last.isPresent()) {
-            model.addAttribute("payments", Collections.EMPTY_LIST);
-            model.addAttribute("name", name);
-            model.addAttribute("collected", "null");
-            model.addAttribute("prop", "null");
-            model.addAttribute("summ", "null");
-            model.addAttribute("methods", "указанных реквизитов нет");
-            model.addAttribute("paidError", "Сборов нет");
-            return new ModelAndView("fastlevy", model);
-        }
-
         model.addAttribute("prop", last.get().getProp());
         model.addAttribute("collected", paymentRepository.amountSum(last.get().getId()).orElse(0));
         model.addAttribute("name", name);
         model.addAttribute("summ", levyRepository.findFirstByOrderByIdDesc().get().getSumm());
         model.addAttribute("methods", last.get().getMethods());
+
+        if (!last.isPresent()) {
+            model.addAttribute("payments", Collections.EMPTY_LIST);
+            model.addAttribute("paidError", "Сборов нет");
+            return new ModelAndView("fastlevy", model);
+        }
+
         return new ModelAndView("paid", model);
     }
 
@@ -56,7 +51,7 @@ public class PaidController {
         Optional<Levy> last = levyRepository.findFirstByOrderByIdDesc();
         if (last.isPresent()) {
             model.addAttribute("prop", last.get().getProp());
-            model.addAttribute("collected", paymentRepository.amountSum(last.get().getId()).orElse(0));
+            model.addAttribute("collected", paymentRepository.amountSum(last.get().getId()).orElse(0) + levyRepository.findFirstByOrderByIdDesc().get().getSumm());
         }
 
         model.addAttribute("name", name);
